57927ceeaf318dd1d7b613f1bf8c3ed894f80f3a
Royk8 <roy.maestre@udea.edu.co>
2024-03-27 16:21:57 +0100

    Handshake classes added. The implementation on the websocket connection will wait

--- s2-typescript-consumer/node_modules/s2-typescript/src/index.ts
+++ s2-typescript-consumer/node_modules/s2-typescript/src/index.ts
@@ -1,4 +1,6 @@
 export {init} from './app'  //Exports the method to be use in another apps.
 export {WebSocketServerController} from './WebSocketServerController' //Exports the method to be use in another apps.
 export {WebSocketClientController} from './WebSocketClientController' //Exports the method to be use in another apps.
-export {PowerForecastValue, PowerRange} from './common' //Exports the method to be use in another apps.
+export {PowerForecastValue, PowerRange, Handshake, HandshakeResponse} from './common' //Exports the method to be use in another apps.
+export type { CommodityQuantity } from './schemas/generated' //Exports the method to be use in another apps.
+export type { EnergyManagementRole } from './messages/generated' //Exports the method to be use in another apps.
---
+++ s2-typescript/src/messages/generated/index.ts
@@ -0,0 +1,2 @@
+export type { Handshake, EnergyManagementRole, ID } from './Handshake';
+export type { HandshakeResponse} from './HandshakeResponse';
