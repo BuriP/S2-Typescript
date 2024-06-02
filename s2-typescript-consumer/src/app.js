93689fb2c82eea972cfd635172500e14688df3c3
Royk8 <roy.maestre@udea.edu.co>
2024-06-02 14:42:34 +0200

    Old repo

--- s2-typescript-consumer/src/app.js
+++ s2-typescript-consumer/src/app.js
@@ -18,9 +18,16 @@ var parseResponse = function (message) {
 (0, s2_typescript_1.init)();
 var ws = new s2_typescript_2.WebSocketServerController(8000, parseResponse.bind(this));
 var hs = new s2_typescript_3.Handshake({ message_id: "1", role: "RM" });
+var nr = new s2_typescript_1.NumberRange({ start_of_range: 0, end_of_range: 100 });
+console.log(hs);
+console.log(nr);
 var powerJson = JSON.stringify(hs, null, 2);
+var numberJson = JSON.stringify(nr, null, 2);
+console.log(powerJson);
+console.log(numberJson);
 (0, wait_1.wait)(10).then(function () {
     ws.BroadcastMessage(powerJson);
+    ws.BroadcastMessage(numberJson);
     (0, wait_1.wait)(5).then(function () {
         console.log(response);
     });
